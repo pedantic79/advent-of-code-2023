d76189970247c39bbf8e2d1acaed272d1875401f
Dan Nguyen <pedantic@gmail.com>
2023-12-13 00:27:03 -0600

    day13: Reordering of code

--- src/day13.rs
+++ src/day13.rs
@@ -1,13 +1,64 @@
+use std::convert::Infallible;
+
 use aoc_runner_derive::{aoc, aoc_generator};
+
+use crate::common::utils::parse_split;
 
 #[derive(Debug)]
 pub struct Pattern {
     pattern: Vec<Vec<u8>>,
 }
 
-fn parse(s: &str) -> Pattern {
-    let pattern: Vec<_> = s.lines().map(|line| line.as_bytes().to_vec()).collect();
-    Pattern { pattern }
+impl Pattern {
+    fn find_mirror<const TARGET: usize>(&self) -> Option<usize> {
+        let a = Self::find_mirror_horz::<TARGET>(&self.pattern);
+
+        if let Some(a) = a {
+            Some(a * 100)
+        } else {
+            Self::find_mirror_vert::<TARGET>(&self.pattern)
+        }
+    }
+
+    fn count_badness(left: &[u8], right: &[u8]) -> usize {
+        left.iter()
+            .zip(right.iter())
+            .filter(|(l, r)| l != r)
+            .count()
+    }
+
+    // fn check_mirror(left: &[Vec<u8>], right: &[Vec<u8>]) -> bool {
+    //     left.iter().rev().zip(right.iter()).all(|(l, r)| l == r)
+    // }
+
+    fn check_mirror(left: &[Vec<u8>], right: &[Vec<u8>]) -> usize {
+        left.iter()
+            .rev()
+            .zip(right.iter())
+            .map(|(l, r)| Self::count_badness(l, r))
+            .sum()
+    }
+
+    fn find_mirror_horz<const TARGET: usize>(p: &[Vec<u8>]) -> Option<usize> {
+        (1..p.len()).find(|&split| {
+            let (left, right) = p.split_at(split);
+            Self::check_mirror(left, right) == TARGET
+        })
+    }
+
+    fn find_mirror_vert<const TARGET: usize>(p: &[Vec<u8>]) -> Option<usize> {
+        let p = rotate(p);
+        Self::find_mirror_horz::<TARGET>(&p)
+    }
+}
+
+impl std::str::FromStr for Pattern {
+    type Err = Infallible;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let pattern: Vec<_> = s.lines().map(|line| line.as_bytes().to_vec()).collect();
+        Ok(Pattern { pattern })
+    }
 }
 
 fn rotate(matrix: &[Vec<u8>]) -> Vec<Vec<u8>> {
@@ -25,59 +76,19 @@ fn rotate(matrix: &[Vec<u8>]) -> Vec<Vec<u8>> {
     rotated_matrix
 }
 
-// fn check_mirror(left: &[Vec<u8>], right: &[Vec<u8>]) -> bool {
-//     left.iter().rev().zip(right.iter()).all(|(l, r)| l == r)
-// }
-
-fn count_badness(left: &[u8], right: &[u8]) -> usize {
-    left.iter()
-        .zip(right.iter())
-        .filter(|(l, r)| l != r)
-        .count()
-}
-
-fn check_mirror(left: &[Vec<u8>], right: &[Vec<u8>]) -> usize {
-    left.iter()
-        .rev()
-        .zip(right.iter())
-        .map(|(l, r)| count_badness(l, r))
-        .sum()
-}
-
-fn find_mirror_horz<const TARGET: usize>(p: &[Vec<u8>]) -> Option<usize> {
-    (1..p.len()).find(|&split| {
-        let (left, right) = p.split_at(split);
-        check_mirror(left, right) == TARGET
-    })
-}
-
-fn find_mirror_vert<const TARGET: usize>(p: &[Vec<u8>]) -> Option<usize> {
-    find_mirror_horz::<TARGET>(&rotate(p))
-}
-
-fn find_mirror<const TARGET: usize>(p: &Pattern) -> Option<usize> {
-    let a = find_mirror_horz::<TARGET>(&p.pattern);
-
-    if let Some(a) = a {
-        Some(a * 100)
-    } else {
-        find_mirror_vert::<TARGET>(&p.pattern)
-    }
-}
-
 #[aoc_generator(day13)]
 pub fn generator(input: &str) -> Vec<Pattern> {
-    input.split("\n\n").map(parse).collect()
+    parse_split(input, "\n\n")
 }
 
 #[aoc(day13, part1)]
 pub fn part1(inputs: &[Pattern]) -> usize {
-    inputs.iter().filter_map(find_mirror::<0>).sum()
+    inputs.iter().filter_map(Pattern::find_mirror::<0>).sum()
 }
 
 #[aoc(day13, part2)]
 pub fn part2(inputs: &[Pattern]) -> usize {
-    inputs.iter().filter_map(find_mirror::<1>).sum()
+    inputs.iter().filter_map(Pattern::find_mirror::<1>).sum()
 }
 
 #[cfg(test)]
@@ -105,8 +116,8 @@ mod tests {
         let p = generator(SAMPLE);
         // println!("{:?}", p);
 
-        assert_eq!(find_mirror_vert::<0>(&p[0].pattern), Some(5));
-        assert_eq!(find_mirror_horz::<0>(&p[1].pattern), Some(4));
+        assert_eq!(Pattern::find_mirror_vert::<0>(&p[0].pattern), Some(5));
+        assert_eq!(Pattern::find_mirror_horz::<0>(&p[1].pattern), Some(4));
     }
 
     #[test]
