479a70f2096b7f7ce75524c34242565177557744
Dan Nguyen <pedantic@gmail.com>
2023-12-13 18:02:56 -0600

    common: Add rotate_*_m_n for MxN matrix rotations

--- src/common/matrix.rs
+++ src/common/matrix.rs
@@ -67,3 +67,57 @@ where
         row.as_mut().reverse();
     }
 }
+
+pub fn rotate_right_m_n<T, A>(a: &[A]) -> Vec<Vec<T>>
+where
+    T: Default + Copy,
+    A: AsRef<[T]>,
+{
+    let rows = a.len();
+    let cols = a[0].as_ref().len();
+
+    let mut rotated: Vec<Vec<T>> = vec![vec![T::default(); rows]; cols];
+    for i in 0..rows {
+        for (j, &v) in a[rows - 1 - i].as_ref().iter().enumerate() {
+            rotated[j][i] = v;
+        }
+    }
+
+    rotated
+}
+
+pub fn rotate_left_m_n<T, A>(a: &[A]) -> Vec<Vec<T>>
+where
+    T: Default + Copy,
+    A: AsRef<[T]>,
+{
+    let rows = a.len();
+    let cols = a[0].as_ref().len();
+
+    let mut rotated = vec![vec![T::default(); rows]; cols];
+    for (i, row) in a.iter().enumerate() {
+        for j in 0..cols {
+            rotated[cols - 1 - j][i] = row.as_ref()[j];
+        }
+    }
+
+    rotated
+}
+
+pub fn rotate_bottom_m_n<T, A>(a: &[A]) -> Vec<Vec<T>>
+where
+    T: Default + Copy,
+    A: AsRef<[T]>,
+{
+    let rows = a.len();
+    let cols = a[0].as_ref().len();
+
+    let mut rotated = vec![vec![T::default(); cols]; rows];
+    for i in 0..rows {
+        for j in 0..cols {
+            rotated[rows - 1 - i][cols - 1 - j] = a[i].as_ref()[j];
+        }
+    }
+
+    rotated
+}
