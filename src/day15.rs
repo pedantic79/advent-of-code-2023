884140b01d826d1b4fe6f3820126afdd49d307d5
Dan Nguyen <pedantic@gmail.com>
2023-12-15 00:20:00 -0600

    day15: cleanup

--- src/day15.rs
+++ src/day15.rs
@@ -14,6 +14,7 @@ fn calc_hash(s: &str) -> usize {
 }
 
 // #[aoc_generator(day15)]
+// this is here so the tests don't need to refer directly to part1 and part2 without the generator
 pub fn generator(s: &str) -> &str {
     s
 }
@@ -25,36 +26,34 @@ pub fn part1(input: &str) -> usize {
 
 #[aoc(day15, part2)]
 pub fn part2(inputs: &str) -> usize {
-    let mut boxes: [_; 256] = std::array::from_fn(|_| Vec::new());
-    'outer: for operation in inputs.split(',') {
+    let mut boxes: [_; 256] = std::array::from_fn(|_| Vec::with_capacity(8));
+    for operation in inputs.split(',') {
         if let Some((label, focal_length)) = operation.split_once('=') {
-            let focal_length = focal_length.parse::<usize>().unwrap();
+            let focal_length: usize = focal_length.parse().unwrap();
             let label_num = calc_hash(label);
-            for (l, p) in boxes[label_num].iter_mut() {
-                if *l == label {
-                    *p = focal_length;
-                    continue 'outer;
-                }
-            }
 
-            // Was not found
-            boxes[label_num].push((label, focal_length));
+            if let Some(p) = boxes[label_num]
+                .iter_mut()
+                .find_map(|(l, p)| (*l == label).then_some(p))
+            {
+                *p = focal_length;
+            } else {
+                boxes[label_num].push((label, focal_length));
+            }
         } else if let Some(label) = operation.strip_suffix('-') {
             let label_num = calc_hash(label);
             if let Some(pos) = boxes[label_num].iter().position(|x| x.0 == label) {
                 boxes[label_num].remove(pos);
             }
+        } else {
+            panic!("unknown input `{operation}`");
         }
     }
 
     let mut sum = 0;
     for (box_num, v) in boxes.iter().enumerate() {
-        if v.is_empty() {
-            continue;
-        }
-
-        for (slot, (_, focal)) in v.iter().enumerate() {
-            sum += (box_num + 1) * (slot + 1) * focal;
+        for (slot_num, (_, focal)) in v.iter().enumerate() {
+            sum += (box_num + 1) * (slot_num + 1) * focal;
         }
     }
 
