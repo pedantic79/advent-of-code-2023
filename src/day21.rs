79cfccd02cbdcbd20c9c7ad99821a862cf103580
Dan Nguyen <pedantic@gmail.com>
2024-01-05 17:43:50 -0600

    day21: Simulate part2 correctly

--- src/day21.rs
+++ src/day21.rs
@@ -1,7 +1,7 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, isize};
 
+use ahash::{HashSet, HashSetExt};
 use aoc_runner_derive::{aoc, aoc_generator};
-use bit_set::BitSet;
 
 #[derive(PartialEq, Eq)]
 pub enum State {
@@ -22,44 +22,57 @@ impl Debug for State {
 
 #[derive(PartialEq, Eq)]
 pub struct Grid {
-    grid: Vec<State>,
-    width: usize,
-    height: usize,
-    start: usize,
+    grid: Vec<Vec<State>>,
+    width: isize,
+    height: isize,
+    start: (isize, isize),
 }
 
 impl Grid {
-    pub fn display(&self, locations: &BitSet) {
-        let mut target = self.width - 1;
+    fn get(&self, index: (isize, isize)) -> Option<&State> {
+        let r = usize::try_from(index.0.rem_euclid(self.height)).unwrap();
+        let c = usize::try_from(index.1.rem_euclid(self.width)).unwrap();
 
-        for (idx, c) in self.grid.iter().enumerate() {
-            if locations.contains(idx) {
-                print!("O");
-            } else {
-                print!("{:?}", c);
+        self.grid.get(r).and_then(|row| row.get(c))
+    }
+
+    pub fn display(&self, locations: &HashSet<(isize, isize)>) {
+        let ((min_y, min_x), (max_y, max_x)) = locations.iter().fold(
+            ((isize::MAX, isize::MAX), (isize::MIN, isize::MIN)),
+            |(min, max), loc| {
+                (
+                    (min.0.min(loc.0), min.1.min(loc.1)),
+                    (max.0.max(loc.0), max.1.max(loc.1)),
+                )
+            },
+        );
+
+        for row in min_y..=max_y {
+            for col in min_x..=max_x {
+                if locations.contains(&(row, col)) {
+                    print!("O");
+                } else {
+                    let c: &State = self.get((row, col)).unwrap();
+                    print!("{:?}", c);
+                }
             }
-            if idx == target {
-                target += self.width;
-                println!();
-            }
+            println!();
         }
+
+        println!();
+        println!();
     }
 
-    fn neighbors(&self, index: usize) -> [usize; 4] {
-        [
-            index + 1,
-            index.saturating_sub(1),
-            index + self.width,
-            index.saturating_sub(self.width),
-        ]
+    fn neighbors(&self, (r, c): (isize, isize)) -> [(isize, isize); 4] {
+        [(r + 1, c), (r - 1, c), (r, c + 1), (r, c - 1)]
     }
 
-    fn step(&self, locations: &BitSet) -> BitSet {
-        let mut res = BitSet::with_capacity(self.grid.len());
+    fn step(&self, locations: &HashSet<(isize, isize)>) -> HashSet<(isize, isize)> {
+        let mut res = HashSet::with_capacity(self.grid.len());
 
-        for index in locations.iter() {
+        for &index in locations.iter() {
             for neigh in self.neighbors(index) {
-                if let Some(State::Plot) = self.grid.get(neigh) {
+                if let Some(State::Plot) = self.get(neigh) {
                     res.insert(neigh);
                 }
             }
@@ -79,32 +92,41 @@ pub fn generator(input: &str) -> Grid {
     for l in input.lines() {
         width = l.len();
 
-        grid.extend(l.bytes().enumerate().map(|(cidx, b)| match b {
-            b'#' => State::Rock,
-            b'.' => State::Plot,
-            b'S' => {
-                start = (height, cidx);
-                State::Plot
-            }
-            _ => panic!("unexpected byte"),
-        }));
+        grid.push(
+            l.bytes()
+                .enumerate()
+                .map(|(cidx, b)| match b {
+                    b'#' => State::Rock,
+                    b'.' => State::Plot,
+                    b'S' => {
+                        start = (height, cidx);
+                        State::Plot
+                    }
+                    _ => panic!("unexpected byte"),
+                })
+                .collect(),
+        );
         height += 1;
     }
 
     Grid {
         grid,
-        height,
-        width,
-        start: start.0 * width + start.1,
+        height: isize::try_from(height).unwrap(),
+        width: isize::try_from(width).unwrap(),
+        start: (
+            isize::try_from(start.0).unwrap(),
+            isize::try_from(start.1).unwrap(),
+        ),
     }
 }
 
-fn solve<const STEPS: usize>(inputs: &Grid) -> usize {
-    let mut taken = BitSet::with_capacity(inputs.grid.len());
+fn solve(inputs: &Grid, steps: usize) -> usize {
+    let mut taken = HashSet::with_capacity(inputs.grid.len());
     taken.insert(inputs.start);
 
-    for _ in 0..STEPS {
+    for _ in 0..steps {
         taken = inputs.step(&taken);
+        // inputs.display(&taken);
     }
 
     taken.len()
@@ -112,12 +134,21 @@ fn solve<const STEPS: usize>(inputs: &Grid) -> usize {
 
 #[aoc(day21, part1)]
 pub fn part1(inputs: &Grid) -> usize {
-    solve::<64>(inputs)
+    solve(inputs, 64)
 }
 
-// #[aoc(day21, part2)]
-pub fn part2(_inputs: &Grid) -> usize {
-    unimplemented!()
+#[aoc(day21, part2)]
+pub fn part2(inputs: &Grid) -> usize {
+    let n = inputs.grid.len();
+    let rem = 26_501_365 % n;
+
+    let xs = [0, 1, 2];
+    let mut ys = Vec::with_capacity(3);
+    for i in [rem, rem + n, rem + n * 2] {
+        let y = solve(inputs, i);
+        ys.push(y as f64);
+    }
+    0
 }
 
 #[cfg(test)]
@@ -140,7 +171,7 @@ mod tests {
     pub fn input_test() {
         // println!("{:?}", generator(SAMPLE));
         let grid = generator(SAMPLE);
-        let mut set = BitSet::new();
+        let mut set = HashSet::new();
         set.insert(grid.start);
         grid.display(&set);
 
@@ -149,11 +180,17 @@ mod tests {
 
     #[test]
     pub fn part1_test() {
-        assert_eq!(solve::<6>(&generator(SAMPLE)), 16);
+        assert_eq!(solve(&generator(SAMPLE), 6), 16);
     }
 
     #[test]
     pub fn part2_test() {
+        assert_eq!(solve(&generator(SAMPLE), 6), 16);
+        assert_eq!(solve(&generator(SAMPLE), 10), 50);
+        assert_eq!(solve(&generator(SAMPLE), 50), 1594);
+        assert_eq!(solve(&generator(SAMPLE), 100), 6536);
+        assert_eq!(solve(&generator(SAMPLE), 500), 167004);
+
         // assert_eq!(part2(&generator(SAMPLE)), 336);
     }
 
